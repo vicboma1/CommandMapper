cc82b94e083f8818427d7936ee4c4b91a59be1f9
Victor Bolinches <vicboma1@users.noreply.github.com>
2015-10-08 13:15:47 +0200

    delete duplicated folder

--- src/main/java/dsl/CommandCollection.java
+++
@@ -1,8 +0,0 @@
-package dsl;
-
-/**
- * Created by vicboma on 07/10/15.
- */
-public interface CommandCollection {
-    int size();
-}
--- src/main/java/dsl/CommandConfigurator.java
+++
@@ -1,8 +0,0 @@
-package dsl;
-
-/**
- * Created by vicboma on 07/10/15.
- */
-public interface CommandConfigurator {
-    CommandConfigurator once(boolean value);
-}
